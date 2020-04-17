36e9ef3ebb6dfda6f50a87579ce2da0de564c2b8
runrey <ernur.bolatkanov@yandex.ru>
2020-04-17 14:02:00 +0600

    changed by runrey2

--- src/astanait/it/Main.java
+++ src/astanait/it/Main.java
@@ -6,5 +6,15 @@ public class Main {
 		System.out.println("Hello World!");
 
 	}
+	
+	public void runrey() {
+		System.out.println("Changed by runrey!");
+	}
+	public void runre2y() {
+		System.out.println("Changed by runr2ey!");
+	}
+	public void runrd2y() {
+		System.out.println("Changed by runr2ey!");
+	}
 
 }
